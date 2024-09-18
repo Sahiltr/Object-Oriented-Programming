6784c0351715fc057b64f1f3de8a8ce8478f730f
Sahil Thakur <thakursahil1912@gmail.com>
2024-09-18 14:25:44 +0530

    Practice

---
+++ prac1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+ussing namespace std;
+
+class Parent{
+    private:
+    int x;
+    public:
+    Parent(){};
+
+    Parent(int x, int y){
+        return a = x+y;
+    }
+};
+
+class Child : public Parent{
+    public:
+    Child(int x, int y, int m, int n): Parent(x,y){
+        
+    }
+}
